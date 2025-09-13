cc5290b9147690a684820914fd26e9305eb3ac32
Xunjian Yin <94303247+xunjianyin@users.noreply.github.com>
2025-09-13 14:55:33 -0400

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,4 +1,6 @@
 srun -p gpu-common --gres=gpu:1 --pty bash -i
 
 tmux new -s my_slurm_session
-srun --job-name=interactive_session --cpus-per-task=4 --mem=16G --time=30-00:00:00 --pty bash
+srun --partition=compsci --job-name=interactive_compsci --cpus-per-task=4 --mem=16G --time=7-00:00:00 --pty bash
+
+squeue -u xy200
